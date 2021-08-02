960b876049287ebbfb38c318383425184d7bad90
daleinen7 <daleinen@gmail.com>
2021-08-02 08:44:06 -0500

    image appears

--- src/components/RadioShow.jsx
+++ src/components/RadioShow.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
+import ShowCard from './ShowCard'
 
 export default function RadioShow() {
   return (
@@ -10,9 +11,7 @@ export default function RadioShow() {
             nodes {
               name
               image {
-                fluid {
-                  src
-                }
+                gatsbyImageData(placeholder: BLURRED)
               }
             }
           }
@@ -24,6 +23,12 @@ export default function RadioShow() {
             <h2>Weird Radio Shows</h2>
             <Link to="/shows">View All</Link>
           </header>
+          <p role="doc-subtitle">Your daily dose of weird</p>
+          <ul>
+            {data.allContentfulRadioShow.nodes.map((show, idx) => {
+              return <ShowCard show={show} key={idx} />
+            })}
+          </ul>
         </section>
       )}
     />
--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-export default function ShowCard() {
+export default function ShowCard({ show }) {
+  console.log(show.image)
+  const image = getImage(show.image)
   return (
-    <>
-      <h2>Individual Show Card</h2>
-    </>
+    <li className="show-card">
+      <GatsbyImage image={image} alt={show.name} imgClassName="show-card-img" />
+      <h3>{show.name}</h3>
+    </li>
   )
 }
