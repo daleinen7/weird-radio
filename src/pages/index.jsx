7f43eab35cee8a81bff5183dbe51e392a8ade0cd
daleinen7 <daleinen@gmail.com>
2021-08-02 07:58:20 -0500

    integrate both components in project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,14 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-<<<<<<< HEAD
 import RadioShow from '../components/RadioShow'
-=======
 import FeaturedArtists from '../components/FeaturedArtists'
->>>>>>> main
 import '../scss/style.scss'
 
 const IndexPage = () => {
   return (
     <Layout>
-<<<<<<< HEAD
-      <RadioShow />
-=======
       <FeaturedArtists />
->>>>>>> main
+      <RadioShow />
     </Layout>
   )
 }
