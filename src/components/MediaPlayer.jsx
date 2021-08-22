2808c6214b9950d219103602ada45a5d8df0e4e8
daleinen7 <daleinen@gmail.com>
2021-08-22 17:46:53 -0500

    art and info

--- src/components/MediaPlayer.jsx
+++ src/components/MediaPlayer.jsx
@@ -1,35 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import play from '../images/play.svg'
+
+const appendScript = (srcURL, selector) => {
+  const script = document.createElement('script')
+  script.src = srcURL
+  script.async = true
+  document.querySelector(selector).appendChild(script)
+  console.log('This fucking script', script)
+}
 
 export default function MediaPlayer() {
   const [radio, setRadio] = useState(false)
   const [audio] = useState(new Audio('https://s3.radio.co/sa333a8356/listen'))
 
   const audioControls = () => {
-    let isPlaying = radio
-
-    if (isPlaying) {
-      audio.pause()
-    } else {
-      audio.play()
-    }
-
-    setRadio(!isPlaying)
+    radio ? audio.pause() : audio.play()
+    setRadio(!radio)
   }
+
+  useEffect(() => {
+    // song info
+    appendScript(
+      'https://embed.radio.co/embed/sa333a8356/song.js',
+      '#radioco_song'
+    )
+    // art
+    appendScript(
+      'https://embed.radio.co/embed/sa333a8356/artwork.js',
+      '#radioco_artwork'
+    )
+  }, [])
 
   return (
     <div className="player">
-      <script src="https://embed.radio.co/embed/sa333a8356/artwork.js"></script>
       <div className="info">
-        <div className="radioco_artwork"></div>
+        <div id="radioco_artwork"></div>
         <div className="text">
           <div>
             <div className="now-playing">Now Playing</div>
             <div className="weird-live">Weird Live!</div>
           </div>
           <div>
-            <div className="song">Song Playing</div>
-            <div className="artist">Artist Playing</div>
+            <div id="radioco_song"></div>
+            {/* <div className="artist">Artist Playing</div> */}
           </div>
         </div>
       </div>
