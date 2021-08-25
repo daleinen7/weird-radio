83b1de4e9ef7b235e1f37e8b05545228b302f023
marialaina <pmarialaina@gmail.com>
2021-08-25 10:28:11 -0600

    adding the links to the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import MediaPlayer from './MediaPlayer'
+import RadioShowDetails from './RadioShowDetail'
+import { Link, StaticQuery, graphql } from 'gatsby'
 
 export default function Header() {
   return (
     <>
+    
+      <Link to="/">ARTISTS</Link>
+      <Link to="/">RADIO SHOWS</Link>
+      <Link to="/">ABOUT US</Link>
+      <input type="text" placeholder="Search.." name="search" />
+      <button type="submit"><i class="fa fa-search"></i>press</button>
+      
+     
       <MediaPlayer />
+      
+     
     </>
   )
 }
