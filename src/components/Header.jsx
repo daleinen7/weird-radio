0c492085e79fa88913440b0515330f40fe19dc4d
marialaina <pmarialaina@gmail.com>
2021-08-27 15:23:15 -0600

    changed the div to a header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,13 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 export default function Header() {
   return (
     <>
-    
+    <header>
       <Link to="/">ARTISTS</Link>
       <Link to="/">RADIO SHOWS</Link>
       <Link to="/">ABOUT US</Link>
       <input type="text" placeholder="Search.." name="search" />
-      <button type="submit"><i class="fa fa-search"></i>press</button>
-      
-     
+      <button type="submit"><i class="fa fa-search"></i>B</button>
+    </header>
       <MediaPlayer />
       
      
