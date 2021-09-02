216da12a873cd6d0d2c2afa57b9586aeb8baabf1
daleinen7 <daleinen@gmail.com>
2021-09-02 15:37:41 -0500

    support section built

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Support() {
   return (
-    <>
+    <section className="support">
       <h2>Support listener-powered Weird Radio</h2>
       <a href="#">Donate Here</a>
-    </>
+    </section>
   )
 }
