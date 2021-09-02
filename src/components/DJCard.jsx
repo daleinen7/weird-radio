869c94c0b6fcd44aa34b4583b131006b26ef352a
daleinen7 <daleinen@gmail.com>
2021-09-02 15:23:33 -0500

    style dj discover section

--- src/components/DJCard.jsx
+++ src/components/DJCard.jsx
@@ -6,9 +6,9 @@ export default function DJCard({ image, name }) {
   const img = getImage(image)
   console.log(img)
   return (
-    <>
+    <div className="dj-card">
       <h2>{name}</h2>
       <GatsbyImage image={img} alt={name} />
-    </>
+    </div>
   )
 }
