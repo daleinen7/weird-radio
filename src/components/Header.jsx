db105cd3b7fc2229f012e889717d7608f590b97b
marialaina <pmarialaina@gmail.com>
2021-09-06 12:28:04 -0600

    removing the search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,16 @@ import { Link, StaticQuery, graphql } from 'gatsby'
 export default function Header() {
   return (
     <>
-    <header>
-      <Link to="/">ARTISTS</Link>
-      <Link to="/">RADIO SHOWS</Link>
-      <Link to="/">ABOUT US</Link>
-      <input type="text" placeholder="Search.." name="search" />
-      <button type="submit"><i class="fa fa-search"></i>B</button>
-    </header>
+    <header className="mainHeader">
+      <img className="logo" src="https://res.cloudinary.com/dj7bt3ah7/image/upload/c_scale,h_132,w_132/v1630886269/Screen_Shot_2021-09-05_at_5.51.29_PM_scwjec.png" />
+      <Link to="/artists" className="link">ARTISTS</Link>
+      <Link to="/shows" className="link">RADIO SHOWS</Link>
+      <Link to="/about" className="link">ABOUT US</Link>
+      {/* <input className="link" type="text" placeholder="Search.." name="search" />
+      <button type="submit"><i class="fa fa-search"></i>B</button> */}
       <MediaPlayer />
+    </header>
       
-     
     </>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,10 +4,10 @@ import Footer from '../components/Footer'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="mainContainer" >
       <Header />
       {children}
       <Footer />
-    </>
+    </div >
   )
 }
