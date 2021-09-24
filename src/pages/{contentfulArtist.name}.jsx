3a5bb8f1c1bfd4cfa594c3dd62ca58b61cd525cb
daleinen7 <daleinen@gmail.com>
2021-09-24 14:41:54 -0500

    start artist show page

--- src/pages/{contentfulArtist.name}.jsx
+++ src/pages/{contentfulArtist.name}.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export default function Artist({ data }) {
+  const artist = data.contentfulArtist;
   return (
     <Layout>
-      <h1>Artist page</h1>
-      <h2>{data.contentfulArtist.name}</h2>
+      <section id="artist">
+        <GatsbyImage image={getImage(artist.image)} alt={artist.name}/>
+        <div className="artist-info">
+          <header>
+            <h1>{artist.name}</h1>
+            <div className="divider"></div>
+            <p role="doc-subtitle">Artist</p>
+          </header>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro iusto qui nulla natus error, tempore sit est facilis quisquam eaque id expedita quam consectetur consequatur aliquam, tenetur vitae odio ea.</p>
+        </div>
+      </section>
     </Layout>
   )
 }
