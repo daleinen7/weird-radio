e436a9c5f2b39bba0b8397809ceb25b486ddb128
daleinen7 <daleinen@gmail.com>
2021-10-08 15:46:33 -0500

    artist bio displays

--- src/pages/{contentfulArtist.name}.jsx
+++ src/pages/{contentfulArtist.name}.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 export default function Artist({ data }) {
-  const artist = data.contentfulArtist;
+  const artist = data.contentfulArtist
   return (
     <Layout>
       <section id="artist">
-        <GatsbyImage image={getImage(artist.image)} alt={artist.name}/>
+        <GatsbyImage image={getImage(artist.image)} alt={artist.name} />
         <div className="artist-info">
           <header>
             <h1>{artist.name}</h1>
             <div className="divider"></div>
             <p role="doc-subtitle">Artist</p>
           </header>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro iusto qui nulla natus error, tempore sit est facilis quisquam eaque id expedita quam consectetur consequatur aliquam, tenetur vitae odio ea.</p>
+          <p>{renderRichText(artist.bio)}</p>
         </div>
       </section>
     </Layout>
