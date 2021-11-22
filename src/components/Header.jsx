8ada73d7e5a6982aff84a104ac898bc4a8c702a9
daleinen7 <daleinen@gmail.com>
2021-11-22 16:31:55 -0600

    start navbar responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/Logo.png'
 import MediaPlayer from './MediaPlayer'
 import { Link } from 'gatsby'
+import menuIcon from '../images/hamburgerMenu.svg'
 
 export default function Header() {
+  const [navMenuOpen, setNavMenuOpen] = useState(false)
+
+  const handleNavMenu = () => {
+    setNavMenuOpen(!navMenuOpen)
+  }
+
   return (
     <>
       <div className="spacer"></div>
       <header id="main">
         <nav>
-          <ul>
+          <button
+            area-label={navMenuOpen ? 'close menu' : 'open menu'}
+            onClick={handleNavMenu}
+          >
+            <img
+              src={menuIcon}
+              alt={navMenuOpen ? 'close menu' : 'open menu'}
+            />
+          </button>
+          <ul className={navMenuOpen && 'nav-menu-open'}>
             <li className={logo}>
               <a href="/">
                 <img src={logo} alt="Weird Radio" />
